7cf5b1aa8facc824f4baa590923b742e9ec02c0c
standy <mostovoyav@gmail.com>
2017-02-16 03:01:28 +0300

    removed uglifyjs beautifier #39

--- gulp/userscript/webpack.js
+++ gulp/userscript/webpack.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
 var header = require('gulp-header');
 var gulpWebpack = require('gulp-webpack');
 var fs = require('fs');
@@ -37,31 +36,6 @@ var outputOptions = {
 };
 
 
-/**
-* uglify нужен чтобы убрать мертвые ветки кода
-* такие как if (false) { ... }
-* */
-var uglifyOptions = {
-	mangle: false,
-	output: {
-		indent_start:   0,     // start indentation on every line (only when `beautify`)
-		indent_level:   4,     // indentation level (only when `beautify`)
-		quote_keys:     false, // quote all keys in object literals?
-		space_colon:    true,  // add a space after colon signs?
-//		ascii_only:     false, // output ASCII-safe? (encodes Unicode characters as ASCII)
-//		inline_script:  false, // escape "</script"?
-		width:          160,    // informative maximum line width (for beautified output)
-//		max_line_len:   32000, // maximum line length (for non-beautified output)
-//		ie_proof:       true,  // output IE-safe code?
-		beautify:       true,  // beautify output?
-		source_map:     null,  // output a source map
-		bracketize:     true,  // use brackets every time?
-		comments:       true,  // output comments?
-		semicolons:     false  // use semicolons to separate statements? (otherwise, newlines)
-	}
-};
-
-
 var HEADER_SRC = './source/export/userscript/header.js';
 var DATA_SRC = './package.json';
 var headerTemplate = fs.readFileSync(HEADER_SRC, 'utf8');
@@ -77,7 +51,6 @@ gulp.task('userscript:webpack', function() {
 				gutil.log('[userscript:webpack]', stats.toString(outputOptions));
 			}
 		))
-		.pipe(uglify(uglifyOptions))
 		.pipe(header(headerTemplate, headerData))
 		.pipe(gulp.dest(DEST));
 });
@@ -93,7 +66,6 @@ gulp.task('watch-userscript:webpack', function() {
 				gutil.log('[watch-userscript:webpack]', stats.toString(outputOptions));
 			}
 		))
-		.pipe(uglify(uglifyOptions))
 		.pipe(header(headerTemplate, headerData))
 		.pipe(gulp.dest(DEST));
 });
