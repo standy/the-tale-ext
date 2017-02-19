a8511bf6efe4a1c26a0a6ed5f33f9ee6554572e1
standy <mostovoyav@gmail.com>
2017-02-19 16:27:40 +0300

    simplify pubsub

--- source/js/utils/pubsub.js
+++ source/js/utils/pubsub.js
@@ -1,30 +1,14 @@
 const _subscribeList = [];
 
-export function publish(e) {
-	const sp = e.split('.');
-	const event = sp[0];
-	const namespace = sp[1];
-	const ctx = this;
+export function publish(eventName, ...args) {
 	for (let i = 0; i < _subscribeList.length; i++) {
-		const s = _subscribeList[i];
-		if ((!namespace || namespace === s.namespace) && (!event || event === s.event)) {
-			s.fn.apply(ctx, Array.prototype.slice.call(arguments, 1));
+		const [sEventName, fn] = _subscribeList[i];
+		if (eventName === sEventName) {
+			fn(...args);
 		}
 	}
 }
 
-export function subscribe(e, fn) {
-	const events = e.split(',');
-	if (events.length > 1) {
-		events.forEach(ev => { subscribe(ev, fn); });
-		return;
-	}
-	const sp = e.split('.');
-	const event = sp[0];
-	const namespace = sp[1];
-	_subscribeList.push({
-		event,
-		namespace,
-		fn,
-	});
+export function subscribe(eventName, fn) {
+	_subscribeList.push([eventName, fn]);
 }
