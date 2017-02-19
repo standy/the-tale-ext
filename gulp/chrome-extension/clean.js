82d6f295f1f48be5c672a6ed3b233125a4adc488
standy <mostovoyav@gmail.com>
2017-02-19 04:47:54 +0300

    removed gulp

--- gulp/chrome-extension/clean.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const rimraf = require('rimraf');
-
-const TASK = 'chrome:clean';
-
-gulp.task(TASK, function(cb) {
-	rimraf('./dist/chrome/', cb);
-});
--- gulp/userscript/clean.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const rimraf = require('rimraf');
-
-const TASK = 'userscript:clean';
-
-gulp.task(TASK, function(cb) {
-	rimraf('./dist/userscript', cb);
-});
